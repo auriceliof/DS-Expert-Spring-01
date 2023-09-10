1baedd28d86412bbaebc2f02f1af5e8efbf9526a
auriceliof <auricelio.suporte@hotmail.com>
2023-09-09 21:12:31 -0300

    01-28 Refatoração no Pageable

--- dscatalog/backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ dscatalog/backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -5,7 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -26,9 +26,9 @@ public class CategoryService {
 	
 	
 	@Transactional(readOnly = true)
-	public Page<CategoryDTO> findAllPaged(PageRequest pagerequest){
+	public Page<CategoryDTO> findAllPaged(Pageable pageable){
 				
-		Page<Category> list = repository.findAll(pagerequest);
+		Page<Category> list = repository.findAll(pageable);
 		
 		return list.map(x -> new CategoryDTO(x)); 
 	}
